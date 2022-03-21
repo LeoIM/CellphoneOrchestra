c18e287054c503fd32343791ca01f2b951b39e98
LeoIM <leo@leoim.com>
2022-03-21 07:53:18 -0400

    described clipSets

--- database.js
+++ database.js
@@ -1,3 +1,7 @@
+// clipSets should be an array of arrays.
+// each sub array should contain a series of objects with the fields "name" and
+// "url". names are a user facing string.
+
 const clipSets = [
     [
         {name:'drumTestA', url:'samples/drumTest.mp3'}
