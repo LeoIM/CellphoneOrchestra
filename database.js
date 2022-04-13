2ca4e2962b1cde198037b9ca5edbf1ea39acf36f
LeoIM <leo@leoim.com>
2022-04-13 04:56:57 -0400

    added hella audio

--- database.js
+++ database.js
@@ -4,13 +4,38 @@
 
 const clipSets = [
     [
-        {name:'OG_DRUMS', url:'samples/BRIGHTERDAYS/BD_OG_DRUMS.wav'},
-        {name:'OG_HEALIT', url:'samples/BRIGHTERDAYS/BD_OG_HEALIT.wav'},
-        {name:'OG_LIFTMEUP', url:'samples/BRIGHTERDAYS/BD_OG_LIFTMEUP.wav'},
-        {name:'OG_OOHOHAHEE', url:'samples/BRIGHTERDAYS/BD_OG_OOHOHAHEE.wav'},
-        {name:'RASHAD_INTRO', url:'samples/BRIGHTERDAYS/BD_RASHAD_INTRO.wav'},
-        {name:'RASHAD_BREAK', url:'samples/BRIGHTERDAYS/BD_RASHAD_BREAK.wav'},
-        {name:'RASHAD_HATS', url:'samples/BRIGHTERDAYS/BD_RASHAD_HATS.wav'},
-        {name:'RASHAD_KEYS', url:'samples/BRIGHTERDAYS/BD_RASHAD_KEYS.wav'}
+        {name:'BD_OG_DRUMS', url:'samples/BRIGHTERDAYS/BD_OG_DRUMS.wav'},
+        {name:'BD_OG_HEALIT', url:'samples/BRIGHTERDAYS/BD_OG_HEALIT.wav'},
+        {name:'BD_OG_LIFTMEUP', url:'samples/BRIGHTERDAYS/BD_OG_LIFTMEUP.wav'},
+        {name:'BD_OG_OOHOHAHEE', url:'samples/BRIGHTERDAYS/BD_OG_OOHOHAHEE.wav'},
+        {name:'BD_RASHAD_INTRO', url:'samples/BRIGHTERDAYS/BD_RASHAD_INTRO.wav'},
+        {name:'BD_RASHAD_BREAK', url:'samples/BRIGHTERDAYS/BD_RASHAD_BREAK.wav'},
+        {name:'BD_RASHAD_HATS', url:'samples/BRIGHTERDAYS/BD_RASHAD_HATS.wav'},
+        {name:'BD_RASHAD_KEYS', url:'samples/BRIGHTERDAYS/BD_RASHAD_KEYS.wav'}
+    ],
+    [
+        {name:'GW_EPIANO', url:'samples/CRYSTALWATERS/GW_EPIANO.wav'},
+        {name:'GW_VERSE', url:'samples/CRYSTALWATERS/GW_VERSE.wav'},
+        {name:'GW_JUSTLIKE', url:'samples/CRYSTALWATERS/GW_JUSTLIKE.wav'},
+        {name:'GW_CHORUS', url:'samples/CRYSTALWATERS/GW_CHORUS.wav'},
+        {name:'GW_PIANO', url:'samples/CRYSTALWATERS/GW_PIANO.wav'},
+    ],
+    [
+        {name:'BIPP_INTRO', url:'samples/BIPP/BIPP_INTRO.wav'},
+        {name:'BIPP_OUTRO', url:'samples/BIPP/BIPP_OUTRO.wav'},
+        {name:'BIPP_VOCAL_A', url:'samples/BIPP/BIPP_VOCAL_A.wav'},
+        {name:'BIPP_VOCAL_B', url:'samples/BIPP/BIPP_VOCAL_B.wav'},
+        {name:'BIPP_VOCAL_C', url:'samples/BIPP/BIPP_VOCAL_C.wav'}
+    ],
+    [
+        {name:'APE_A', url:'samples/SOICHI/APE_A.wav'},
+        {name:'APE_B', url:'samples/SOICHI/APE_B.wav'},
+        {name:'APE_C', url:'samples/SOICHI/APE_C.wav'},
+        {name:'APE_D', url:'samples/SOICHI/APE_D.wav'},
+        {name:'APE_E', url:'samples/SOICHI/APE_E.wav'},
+        {name:'APE_F', url:'samples/SOICHI/APE_F.wav'},
+        {name:'APE_G', url:'samples/SOICHI/APE_G.wav'},
+        {name:'APE_H', url:'samples/SOICHI/APE_H.wav'}
     ]
+
 ];
